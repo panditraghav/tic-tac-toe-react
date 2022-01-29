5957b8dea076d725d729f2fbc1f3bf8c8f55a11a
Raghav Pandit <panditraghav18@gmail.com>
2022-01-29 14:03:01 +0530

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 
-function Footer(){
+function Footer() {
     return (
-            <footer>
+        <footer>
+            <p>Made by Raghav Pandit</p>
+            <div className="contacts">
                 <a href="https://github.com/panditraghav/tic-tac-toe-react.git" target="_blank"><i className="fab fa-github-square"></i>Github</a>
                 <a href="https://www.linkedin.com/in/raghav-pandit-19404b222" target="_blank"><i className="fab fa-linkedin"></i>LinkedIn</a>
-            </footer>
+            </div>
+        </footer>
     )
 }
 
